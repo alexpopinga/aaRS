862cccb391a41d79edbbd1f8186d621d5b7d85ff
Alex Popinga <alexpopinga@gmail.com>
2018-09-10 09:56:41 +1200

    fixed printing

--- aaRSCheck.py
+++ aaRSCheck.py
@@ -5,8 +5,5 @@ with open("AARS.xml", 'r') as file:
 		seq = data[data.find("value")+6:]
 		seqLength = len(seq)-1
 		if (seqLength != 116) & (x > 5):
-			print("On line ")
-			print x
-			print(" the sequence length is ")
-			print seqLength
+			print("On line " + str(x) + " the sequence length is " + str(seqLength))
 		x += 1
