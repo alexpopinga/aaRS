100d378b7331147bd37817283da36738bf9faa67
Alex Popinga <alexpopinga@gmail.com>
2018-09-10 09:51:29 +1200

    more useful script

--- aaRSCheck.py
+++ aaRSCheck.py
@@ -1,7 +1,12 @@
-x = 0
+x = 1
 with open("AARS.xml", 'r') as file:
-	for x in range(0,980):
+	for x in range(1,980):
 		data = file.readline()
-		seq = data[data.find("value")+6:]	
-		print (len(seq)-1)
+		seq = data[data.find("value")+6:]
+		seqLength = len(seq)-1
+		if (seqLength != 116) & (x > 5):
+			print("On line ")
+			print x
+			print(" the sequence length is ")
+			print seqLength
 		x += 1
