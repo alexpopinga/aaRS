f6f4118c697bfb3e50cd4a5088850de51ff4a584
Paul Freeman <paul.freeman.cs@gmail.com>
2018-09-25 08:55:20 +1200

    use Levenshtein distance to find gap sequences from no-gap sequences

--- aars_tools.py
+++ aars_tools.py
@@ -2,7 +2,11 @@
 
 a.k.a. "Paul's tools"
 """
+import sys
+from itertools import product
+import json
 import xml.etree.ElementTree as ET
+import numpy as np
 
 AARS_XML = 'AARS.xml'
 BASE_DIR = 'BEAST 2/XMLs/Better Priors (final, actually used XMLs)/'
@@ -42,39 +46,75 @@ def class_two_sequences():
             for s in data if s.tag == 'sequence'}
 
 
-def is_same_sequence(seq1, seq2):
-    """Check if sequences are basically the same
-
-    Ignores gaps, '?', and trailing characters
-    """
-    if seq1 == "" or seq2 == "":
-        return True
-    if seq1 == seq2:
-        return True
-    if seq1[0] == seq2[0]:
-        return is_same_sequence(seq1[1:], seq2[1:])
-    if seq2[0] == '-' or seq2[0] == '?':
-        return is_same_sequence(seq1, seq2[1:])
-    if seq2[0] == 'V' and is_same_sequence(seq1, seq2[1:]):
-        return True
-    return False
-
-
-def find_matches(seq_name):
-    """Find any gapped sequences matching the named sequence
-
-    uses is_same_sequence(seq1, seq2) to find matches
-    """
+def find_match(seq_name, no_gaps=final_sequences(), gaps=all_sequences()):
+    """Find best gapped sequences matching the named sequence"""
     try:
-        seq1 = final_sequences()[seq_name]
+        source = no_gaps[seq_name]
     except ValueError:
         print('sequences name', seq_name, 'not found in final sequences file')
         return None
-    matches = []
-    for seq2_name, seq2 in all_sequences().items():
-        if is_same_sequence(seq1, seq2):
-            matches.append(seq2_name)
-    return matches
+    best_key, best_value = sorted(
+        gaps.items(),
+        key=(lambda target: levenshtein_distance(source, target[1])))[0]
+    return best_key, levenshtein_distance(source, best_value)
+
+
+def levenshtein_distance(source, target):
+    """Custom Levenshtein distance calculation"""
+    distance_matrix = np.zeros((len(source) + 1, len(target) + 1), int)
+    distance_matrix[0, :] = np.arange(len(target) + 1)
+    distance_matrix[:, 0] = np.arange(len(source) + 1)
+    for i, j in product(range(1, len(source)+1), range(1, len(target)+1)):
+        substitution_cost = 0 if source[i-1] == target[i-1] else 1
+        distance_matrix[i, j] = min(
+            distance_matrix[i-1, j] + 1,
+            distance_matrix[i, j-1] + 1,
+            distance_matrix[i-1, j-1] + substitution_cost
+        )
+    return int(distance_matrix[len(source), len(target)])
+
+
+def make_no_gaps_to_gaps_file():
+    """Write the no gaps to gaps file"""
+    version = 1
+    try:
+        with open('no_gaps_to_gaps.json') as f:
+            data = json.load(f)
+        if data['version'] != version:
+            data = {'version': version}
+    except KeyError:
+        data = {'version': version}
+    except FileNotFoundError:
+        data = {'version': version}
+    except json.decoder.JSONDecodeError:
+        data = {'version': version}
+    no_gaps = final_sequences()
+    gaps = all_sequences()
+    sys.stdout.write('\t0%')
+    sys.stdout.flush()
+    for i, no_gaps_key in enumerate(no_gaps.keys()):
+        try:
+            read_test = {
+                'no-gaps-key': data[no_gaps_key]['no-gaps-key'],
+                'no-gaps-value': data[no_gaps_key]['no-gaps-value'],
+                'gaps-key': data[no_gaps_key]['gaps-key'],
+                'gaps-value': data[no_gaps_key]['gaps-value'],
+                'distance': data[no_gaps_key]['distance']
+            }
+        except KeyError:
+            gaps_key, distance = find_match(
+                no_gaps_key, no_gaps=no_gaps, gaps=gaps)
+            data[no_gaps_key] = {
+                'no-gaps-key': no_gaps_key,
+                'no-gaps-value': no_gaps[no_gaps_key],
+                'gaps-key': gaps_key,
+                'gaps-value': gaps[gaps_key],
+                'distance': distance
+            }
+            with open('no_gaps_to_gaps.json', 'w') as f:
+                json.dump(data, f, indent=2)
+        sys.stdout.write('\r\t{}%'.format((i * 100) // len(no_gaps)))
+        sys.stdout.flush()
 
 
 # DEMO
@@ -120,29 +160,6 @@ if __name__ == "__main__":
             break
         print('\t{}: {}'.format(k, v))
 
-    print('\n\nis_same_sequence(seq1, seq2)')
-    seq1 = 'IIEFSPNIAKPFHAGHLRSTIIGGFLANLIRMNYLGDWGEFSIEKYIDTYARLDVYSGESQLYLTRDVGAAMIYVIASQQDLHAAQFFEILKQMLQHVNFGMSTR'
-    seq2 = 'IIEFSPNIAKPFHAGHLRSTIIGGFLANL-IRMNYLGDWG-EFSIEKYIDTYARLD-VYSGESQ-LYLTRDVGAAM-IYVIASQQDLHAAQFFEILKQM-LQHVNF------'
-    print('\tseq1 is', seq1)
-    print('\tseq2 is', seq2)
-    print('\tresult is:', is_same_sequence(seq1, seq2))
-
     print('\n\nfind_match(seq_name)')
-    count_found = 0
-    count_not_found = 0
-    count_multiple_matches = 0
-    for name in final_sequences().keys():
-        # print('\tfinding match for:', name, final_sequences()[name])
-        found = find_matches(name)
-        if found:
-            count_found += 1
-            if len(found) > 1:
-                count_multiple_matches += 1
-            # for match in found:
-                # print('\t            found:', match, all_sequences()[match], '\n')
-        else:
-            count_not_found += 1
-            # print('\t            found: (no match)\n')
-    print('\tfound:', count_found)
-    print('\tnot found:', count_not_found)
-    print('\tmultiple matches:', count_multiple_matches)
+    make_no_gaps_to_gaps_file()
+    print()
