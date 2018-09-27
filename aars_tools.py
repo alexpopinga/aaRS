90072e093a0287e3e52fcb831d425af6d2c87b5c
Paul Freeman <paul.freeman.cs@gmail.com>
2018-09-27 15:51:15 +1200

    added domain, species, aa, and path to JSON data

--- aars_tools.py
+++ aars_tools.py
@@ -3,7 +3,9 @@
 a.k.a. "Paul's tools"
 """
 import sys
+import os.path
 from itertools import product
+import glob
 import json
 import xml.etree.ElementTree as ET
 import numpy as np
@@ -28,6 +30,7 @@ except ImportError:
 
 AARS_XML = 'AARS.xml'
 BASE_DIR = 'BEAST 2/XMLs/Better Priors (final, actually used XMLs)/'
+A_B_E_ZIP = '../../Documents'
 CLASS_I = BASE_DIR + 'ClassI_betterPriors.xml'
 CLASS_II = BASE_DIR + 'ClassII_betterPriors.xml'
 
@@ -77,47 +80,84 @@ def find_match(seq_name, no_gaps=final_sequences(), gaps=all_sequences()):
     return best_key, levenshtein_distance(source, best_value)
 
 
+def parse_no_gaps_keys(no_gaps_key):
+    """Parse key into domain, species, amino acid"""
+    domain_dict = {'arch': 'Archaea',
+                   'bact': 'Bacteria',
+                   'euk': 'Eukaryotes'}
+    splits = no_gaps_key.split('_')
+    aa = splits[0]
+    try:
+        domain = domain_dict[splits[1]]
+    except KeyError:
+        domain = 'Unknown'
+    for i, text in enumerate(splits):
+        if len(text) == 1 and text[0] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            species = '_'.join(splits[i:])
+            break
+    else:
+        species = splits[-1]
+    return domain, species, aa
+
+
 def make_no_gaps_to_gaps_file():
     """Write the no gaps to gaps file"""
-    version = 3
-    try:
-        with open('no_gaps_to_gaps.json') as f:
-            data = json.load(f)
-        if data['version'] != version:
-            data = {'version': version}
-    except KeyError:
-        data = {'version': version}
-    except FileNotFoundError:
-        data = {'version': version}
-    except json.decoder.JSONDecodeError:
-        data = {'version': version}
     no_gaps = final_sequences()
     gaps = all_sequences()
     sys.stdout.write('\t0%')
     sys.stdout.flush()
+    data = {}
     for i, no_gaps_key in enumerate(no_gaps.keys()):
-        try:
-            read_test = {
-                'no-gaps-key': data[no_gaps_key]['no-gaps-key'],
-                'no-gaps-value': data[no_gaps_key]['no-gaps-value'],
-                'gaps-key': data[no_gaps_key]['gaps-key'],
-                'gaps-value': data[no_gaps_key]['gaps-value'],
-                'distance': data[no_gaps_key]['distance']
-            }
-        except KeyError:
-            gaps_key, distance = find_match(
-                no_gaps_key, no_gaps=no_gaps, gaps=gaps)
-            data[no_gaps_key] = {
-                'no-gaps-key': no_gaps_key,
-                'no-gaps-value': no_gaps[no_gaps_key],
-                'gaps-key': gaps_key,
-                'gaps-value': gaps[gaps_key],
-                'distance': distance
-            }
-            with open('no_gaps_to_gaps.json', 'w') as f:
-                json.dump(data, f, indent=2)
+        gaps_key, distance = find_match(
+            no_gaps_key, no_gaps=no_gaps, gaps=gaps)
+        domain, species, aa = parse_no_gaps_keys(no_gaps_key)
+        path, path_cost = predict_path(domain, species, aa)
+        data[no_gaps_key] = {
+            'no-gaps-key': no_gaps_key,
+            'domain': domain,
+            'species': species,
+            'amino acid': aa,
+            'gaps-key': gaps_key,
+            'no-gaps-value': no_gaps[no_gaps_key],
+            'gaps-value': gaps[gaps_key],
+            'gaps-sequence-uncertainty': distance,
+            'path': path,
+            'path-uncertainty': path_cost}
         sys.stdout.write('\r\t{}%'.format((i * 100) // len(no_gaps)))
         sys.stdout.flush()
+    with open('no_gaps_to_gaps.json', 'w') as f:
+        json.dump(data, f, indent=2)
+
+
+def predict_path(domain, species, aa):
+    """Try to find the path to the original sequences"""
+    base_dir = A_B_E_ZIP + "/Archaeans_Bacteria_Eukaryotes"
+    possibles = glob.glob(base_dir + '/{}/{}*'.format(domain, species[0]))
+    if not possibles:
+        possibles = glob.glob(base_dir + '/{}/*'.format(domain))
+    if not possibles:
+        possibles = glob.glob(base_dir + '/*/{}*'.format(species[0]))
+    if not possibles:
+        possibles = glob.glob(base_dir + '/*/*')
+    best = sorted(possibles, key=lambda target: levenshtein_distance(
+        species.lower(), construct_species(os.path.basename(target)).lower()))
+    return best[0], levenshtein_distance(
+        species.lower(), construct_species(os.path.basename(best[0])).lower())
+
+
+def construct_species(name):
+    """Abbreviate capitalized words to one letter"""
+    splits = name.split(' ')
+    if 'Halobacterium' in splits[0]:
+        return name
+    new = []
+    for i, text in enumerate(splits):
+        if text[0] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+            new.append(str(text[0]))
+        else:
+            new.extend(splits[i:])
+            break
+    return '_'.join(new)
 
 
 # DEMO
@@ -163,6 +203,6 @@ if __name__ == "__main__":
             break
         print('\t{}: {}'.format(k, v))
 
-    print('\n\nfind_match(seq_name)')
+    print('\n\nmake_no_gaps_to_gaps_file() :')
     make_no_gaps_to_gaps_file()
     print()
