e6431a6983f8699c0bce0bd2c8ef3612c5b5ec76
alexpopinga <alexpopinga@gmail.com>
2018-11-25 17:38:45 +1300

    updating script, xml

--- RandomlyThinLeaves.py
+++ RandomlyThinLeaves.py
@@ -2,13 +2,19 @@ import random
 
 x = 1
 y = 1
-text_file = open("output.txt", "w")
+samples = open("10RandomSamples.xml", "w")
 
-with open("IterativeSubMatrix_noKMSKS_20states_SEQUENCES.xml", 'r') as file:
-	for x in range(1,980):
-		data = file.readline()
-		z = random.randint(1,2)
-		if (z < 2):
-			text_file.write(data)
+for y in range(1,10):
+	with open("IterativeSubMatrix_noKMSKS_20states_SEQUENCES.xml", 'r') as file:
+		for x in range(1,961):
+			data = file.readline()
+			z = random.randint(1,2)
+			if (z < 2):
+				samples.write(data)
+	y += 1
+	if (y < 10):
+		samples.write("\n")
+	file.close()
+	x = 1
 				
-text_file.close()
+samples.close()
